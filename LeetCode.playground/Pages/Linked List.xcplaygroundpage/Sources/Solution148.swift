563925291bea25884a1f345f1e3858ec92b8f21d
Eli Pacheco Hoyos <elipachecohoyos@MacBook-Pro-de-Eli.local>
2024-10-18 00:46:22 -0500

    Improve solution 148 with linked list

--- LeetCode.playground/Pages/Linked List.xcplaygroundpage/Sources/Solution148.swift
+++ LeetCode.playground/Pages/Linked List.xcplaygroundpage/Sources/Solution148.swift
@@ -22,106 +22,208 @@ public class ListNode: CustomStringConvertible {
 
 public class Solution148 {
     public static func sortList(_ head: ListNode?) -> ListNode? {
-        if let head {
-            let array = makeArray(from: head)
-            let sortedArray = sort(array)
-            let finalListNode = makeListNode(from: sortedArray)
-
-            return finalListNode
+        guard let head else {
+            return head
         }
-
-        return head
-    }
-
-    static func makeListNode(from array: [Int]) -> ListNode {
-        var lastNode: ListNode?
-        var head: ListNode!
-
-        for value in array {
-            let nodeValue = ListNode(value)
-            
-            if let tempLastNode = lastNode {
-                tempLastNode.next = nodeValue
-            } else {
-                head = nodeValue
-            }
-
-            lastNode = nodeValue
-        }
-
-        return head
-    }
-
-    static func makeArray(from node: ListNode) -> [Int] {
-        var array = Array<Int>()
-        var tempNode: ListNode? = node
-
-        while tempNode != nil {
-            if let val = tempNode?.val {
-                array.append(val)
-            }
-            
-            tempNode = tempNode?.next
-        }
-
-        return array
+        
+        return splitList(head)
     }
     
-    static func sort(_ list: [Int]) -> [Int] {
-        return mergeSort(list: list)
-    }
-
-    static func mergeSort(list: [Int]) -> [Int] {
-        guard list.count > 1 else {
-            return list
+    static func splitList(_ head: ListNode) -> ListNode {
+        guard let (leftList, rightList) = splitNode(head) else {
+            return head
         }
         
-        let mid = list.count / 2
-        let leftHalve = 0..<mid
-        let rightHalve = mid..<list.count
+        let newLeftList = splitList(leftList)
+        let newRightList = splitList(rightList)
         
-        let leftArray = mergeSort(list: Array<Int>(list[leftHalve]))
-        let rightArray = mergeSort(list: Array<Int>(list[rightHalve]))
-        
-        return merge(leftArray, rightArray)
+        return mergeLists(newLeftList, newRightList)
     }
-
-    static func merge(_ leftArray: [Int], _ rightArray: [Int]) -> [Int] {
-        var leftIndex = 0
-        var rightIndex = 0
-        var sortedItems: [Int] = []
-        sortedItems.reserveCapacity(leftArray.count + rightArray.count)
+    
+    static func mergeLists(_ leftList: ListNode, _ rightList: ListNode) -> ListNode {
+        var initialNode = ListNode()
+        var newNode = initialNode
+        var lastLeftNode: ListNode? = leftList
+        var lastRightNode: ListNode? = rightList
         
-        while leftIndex < leftArray.count && rightIndex < rightArray.count {
-            let leftItem = leftArray[leftIndex]
-            let rightItem = rightArray[rightIndex]
-            
-            if leftItem < rightItem {
-                sortedItems.append(leftItem)
-                leftIndex += 1
-            } else if rightItem < leftItem {
-                sortedItems.append(rightItem)
-                rightIndex += 1
+        while (lastLeftNode != nil && lastRightNode != nil) {
+            let resultVal: Int
+            var needsToDuplicate: Bool = false
+            if lastLeftNode!.val < lastRightNode!.val {
+                resultVal = lastLeftNode!.val
+                lastLeftNode = lastLeftNode?.next
+            } else if lastRightNode!.val < lastLeftNode!.val {
+                resultVal = lastRightNode!.val
+                lastRightNode = lastRightNode?.next
             } else {
-                leftIndex += 1
-                rightIndex += 1
-                sortedItems.append(leftItem)
-                sortedItems.append(rightItem)
+                needsToDuplicate = true
+                resultVal = lastLeftNode!.val
+                lastLeftNode = lastLeftNode?.next
+                lastRightNode = lastRightNode?.next
+            }
+            
+            let resultNode = ListNode(resultVal)
+            newNode.next = resultNode
+            
+            if needsToDuplicate {
+                let clonedNode = ListNode(resultVal)
+                resultNode.next = clonedNode
+                newNode = clonedNode
+            } else {
+                newNode = resultNode
             }
         }
         
-        while leftIndex < leftArray.count {
-            let leftItem = leftArray[leftIndex]
-            sortedItems.append(leftItem)
-            leftIndex += 1
+        if let lastLeftNode {
+            let resultNode = ListNode(lastLeftNode.val)
+            resultNode.next = lastLeftNode.next
+            newNode.next = resultNode
         }
         
-        while rightIndex < rightArray.count {
-            let rightItem = rightArray[rightIndex]
-            sortedItems.append(rightItem)
-            rightIndex += 1
+        if let lastRightNode {
+            let resultNode = ListNode(lastRightNode.val)
+            resultNode.next = lastRightNode.next
+            newNode.next = resultNode
         }
         
-        return sortedItems
+        return initialNode.next!
     }
+    
+    static func printNode(_ node: ListNode) {
+        var currentHead: ListNode? = node
+        var size: Int = 0
+        
+        while currentHead != nil {
+            print(currentHead?.val ?? "Invalid value")
+            currentHead = currentHead?.next
+            size += 1
+        }
+        
+        print("On print, node size: \(size)")
+    }
+    
+    static func splitNode(_ originalNode: ListNode?) -> (leftList: ListNode, rightList: ListNode)? {
+        var slowHead: ListNode? = originalNode
+        var fastHead: ListNode? = originalNode
+        
+        while fastHead != nil, fastHead?.next != nil {
+            fastHead = fastHead?.next?.next
+            
+            if fastHead != nil {
+                slowHead = slowHead?.next
+            }
+        }
+        
+        let rightList = slowHead?.next
+        slowHead?.next = nil
+        
+        guard let leftList = originalNode, let rightList else {
+            return nil
+        }
+        
+        return (leftList, rightList)
+    }
+//    public static func sortList(_ head: ListNode?) -> ListNode? {
+//        if let head {
+//            let array = makeArray(from: head)
+//            let sortedArray = sort(array)
+//            let finalListNode = makeListNode(from: sortedArray)
+//
+//            return finalListNode
+//        }
+//
+//        return head
+//    }
+//
+//    static func makeListNode(from array: [Int]) -> ListNode {
+//        var lastNode: ListNode?
+//        var head: ListNode!
+//
+//        for value in array {
+//            let nodeValue = ListNode(value)
+//            
+//            if let tempLastNode = lastNode {
+//                tempLastNode.next = nodeValue
+//            } else {
+//                head = nodeValue
+//            }
+//
+//            lastNode = nodeValue
+//        }
+//
+//        return head
+//    }
+//
+//    static func makeArray(from node: ListNode) -> [Int] {
+//        var array = Array<Int>()
+//        var tempNode: ListNode? = node
+//
+//        while tempNode != nil {
+//            if let val = tempNode?.val {
+//                array.append(val)
+//            }
+//            
+//            tempNode = tempNode?.next
+//        }
+//
+//        return array
+//    }
+//    
+//    static func sort(_ list: [Int]) -> [Int] {
+//        return mergeSort(list: list)
+//    }
+//
+//    static func mergeSort(list: [Int]) -> [Int] {
+//        guard list.count > 1 else {
+//            return list
+//        }
+//        
+//        let mid = list.count / 2
+//        let leftHalve = 0..<mid
+//        let rightHalve = mid..<list.count
+//        
+//        let leftArray = mergeSort(list: Array<Int>(list[leftHalve]))
+//        let rightArray = mergeSort(list: Array<Int>(list[rightHalve]))
+//        
+//        return merge(leftArray, rightArray)
+//    }
+//
+//    static func merge(_ leftArray: [Int], _ rightArray: [Int]) -> [Int] {
+//        var leftIndex = 0
+//        var rightIndex = 0
+//        var sortedItems: [Int] = []
+//        sortedItems.reserveCapacity(leftArray.count + rightArray.count)
+//        
+//        while leftIndex < leftArray.count && rightIndex < rightArray.count {
+//            let leftItem = leftArray[leftIndex]
+//            let rightItem = rightArray[rightIndex]
+//            
+//            if leftItem < rightItem {
+//                sortedItems.append(leftItem)
+//                leftIndex += 1
+//            } else if rightItem < leftItem {
+//                sortedItems.append(rightItem)
+//                rightIndex += 1
+//            } else {
+//                leftIndex += 1
+//                rightIndex += 1
+//                sortedItems.append(leftItem)
+//                sortedItems.append(rightItem)
+//            }
+//        }
+//        
+//        while leftIndex < leftArray.count {
+//            let leftItem = leftArray[leftIndex]
+//            sortedItems.append(leftItem)
+//            leftIndex += 1
+//        }
+//        
+//        while rightIndex < rightArray.count {
+//            let rightItem = rightArray[rightIndex]
+//            sortedItems.append(rightItem)
+//            rightIndex += 1
+//        }
+//        
+//        return sortedItems
+//    }
 }
